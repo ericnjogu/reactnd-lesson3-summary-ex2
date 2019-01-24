6bf0d8401eacab62f1f2ece6d9b76a43119bb3a5
Eric Njogu <kunadawa@gmail.com>
2019-01-24 05:58:02 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,95 +1,82 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-/*
-This exercise will help you practice many of your newly aquired React skills.
-
-The instructions are included in the `instructions.md` file.
-*/
-
-const users = [{ username: 'Amy' }, { username: 'John' }];
-
-const messages = [
-  { username: 'Amy', text: 'Hi, Jon!' },
-  { username: 'Amy', text: 'How are you?' },
-  { username: 'John', text: 'Hi, Amy! Good, you?' },
-];
+import ChatWindow from './ChatWindow'
 
 class App extends Component {
+  
+  state = {
+    users: [{ username: 'Amy' }, { username: 'John' }],
+    inputMessage: {'Amy':'','John':''},
+    //submitted: {'Amy':false, 'John':false},
+    messages : []
+  }
   /*
   If the user did not type anything, he/she should not be
   allowed to submit.
   */
-  isDisabled = () => {
-    return false;
+  isDisabled = (username) => {
+    return this.state.inputMessage[username].trim().length === 0;
+    //return false
   };
 
+updateInputMsg = (event, username) => {
+  const newInput = event.target.value;
+  this.setState({
+    inputMessage: {[username]:newInput}
+  });
+};
+
+	handleSubmit = (event, username) => {
+      event.preventDefault();
+      this.props.userSubmitted = username;
+      /*this.setState({
+        submitted: {[username]:true}
+      });*/
+      /*this.setState(
+        (oldState, props) => ({messages:[...oldState.messages, {[uname]:oldState.inputMessage[uname]}]})
+      );*/
+      this.setState(
+        (oldState, props) => (
+          {
+	          'messages':[...oldState.messages, {'username': props.userSubmitted, 'text': oldState.inputMessage[props.userSubmitted]}]
+          }
+        ),
+         () => {this.props.userSubmitted = ''}
+      );
+    };
+
+/*
+	componentDidUpdate(prevProps, prevState) {
+      const usersSubmitted = prevState.users.filter(user => prevState.submitted[user.username] === true)
+		if (usersSubmitted.length > 0) {
+            this.setState(
+          		{
+                  'messages':[...prevState.messages, usersSubmitted.map(user => ({ 'username': user.username, 'text': prevState.inputMessage[user.username]}))],
+					'submitted':usersSubmitted.map(user => ({[user.username]:false})),
+}
+            );
+        }
+    }
+*/
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
+          <h1 className="App-title">Exercise 2 - All Together</h1>
         </header>
         <div className="container">
-          <div className="chat-window">
-            <h2>Super Awesome Chat</h2>
-            <div className="name sender">{users[0].username}</div>
-
-            <ul className="message-list">
-              {messages.map((message, index) => (
-                <li
-                  key={index}
-                  className={
-                    message.username === users[0].username ? 'message sender' : 'message recipient'
-                  }
-                >
-                  <p>{`${message.username}: ${message.text}`}</p>
-                </li>
-              ))}
-            </ul>
-
-            <div>
-              <form className="input-group">
-                <input type="text" className="form-control" placeholder="Enter your message..." />
-                <div className="input-group-append">
-                  <button className="btn submit-button" disabled={this.isDisabled()}>
-                    SEND
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-
-          <div className="chat-window">
-            <h2>Super Awesome Chat</h2>
-            <div className="name sender">{users[1].username}</div>
-            <ul className="message-list">
-              {messages.map((message, index) => (
-                <li
-                  key={index}
-                  className={
-                    message.username === users[1].username ? 'message sender' : 'message recipient'
-                  }
-                >
-                  <p>{`${message.username}: ${message.text}`}</p>
-                </li>
-              ))}
-            </ul>
-
-            <div>
-              <form className="input-group">
-                <input type="text" className="form-control" placeholder="Enter your message..." />
-                <div className="input-group-append">
-                  <button className="btn submit-button" disabled={this.isDisabled()}>
-                    SEND
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
+          <ChatWindow 
+    			messages={this.state.messages} 
+				user={this.state.users[0]} 
+				submitIsDisabled={this.isDisabled}
+				updateInputMsg = {this.updateInputMsg}
+				handleSubmit = {this.handleSubmit}
+				inputMessage = {this.state.inputMessage[this.state.users[0].username]}
+          />
         </div>
+        
       </div>
     );
   }
